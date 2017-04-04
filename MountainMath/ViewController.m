4af31eb7d8a8dbe7ad0912546788cc4af6893c56
Shin Park <shinpark@shins-mac-mini.home>
2017-04-03 21:25:30 -0500

    Objective-C numbers

--- MountainMath/ViewController.m
+++ MountainMath/ViewController.m
@@ -16,7 +16,36 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    int imAnInt = 5;
+    float iAmAFloat = 3.3;
+    double iAmTheDouble = 5.666664;
+    
+    NSLog(@"Int: %d", imAnInt);
+    NSLog(@"Float: %f", iAmAFloat);
+    NSLog(@"Double: %f", iAmTheDouble);
+    
+    NSNumber *numInt = [NSNumber numberWithInt:5];
+    NSNumber *numInt2 = [NSNumber numberWithInt:6];
+    
+    int val = numInt.intValue;
+    int val2 = numInt2.intValue;
+    
+    int sum = val + val2;
+    
+    NSLog(@"Sum: %d", sum);
+    
+    NSNumber * numSum = [NSNumber numberWithInt:sum];
+    
+    NSArray *arr = @[numInt, numInt2, numSum];
+    
+    NSString *str = numSum.stringValue;
+    
+    NSInteger someInt = 55;
+    
+    NSNumber *sumNum = [NSNumber numberWithInt:(numInt.intValue * numInt2.intValue)];
+    
 }
 
 
